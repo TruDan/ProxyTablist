2519faad3fa89a28b5420735607ecc11fed0f8d1
Scrayos <scrayos@justchunks.net>
2013-06-24 17:41:40 +0200

    Initial Release

---
+++ src/main/java/eu/scrayos/proxytablist/ProxyTablist.java
@@ -0,0 +1,32 @@
+package eu.scrayos.proxytablist;
+
+import eu.scrayos.proxytablist.include.Metrics;
+import java.io.IOException;
+import net.craftminecraft.bungee.bungeeyaml.pluginapi.ConfigurablePlugin;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.api.tab.TabAPI;
+
+public class ProxyTablist extends ConfigurablePlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        ProxyTablistFormatter.addCodes();
+        this.saveDefaultConfig();
+        ProxyServer.getInstance().getPluginManager().registerListener(this, this);
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        }
+        catch (IOException ex) {
+        }
+    }
+
+    @EventHandler
+    public void onServerConnect(ServerConnectEvent e) {
+        ProxyTablistHandler.setSlot e
+        .getPlayer().setDisplayName(ProxyTablistFormatter.nameFormatter(e.getPlayer(), getConfig().getString("prefixes." + e.getTarget().getName())));
+    }
+}
