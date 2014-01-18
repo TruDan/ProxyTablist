a0d604ce418277b882800ffd820ee86d177bb0ac
Scrayos <scrayos@justchunks.net>
2014-01-18 11:44:11 +0100

    Added 2 Color-Codes + Tidied some things up.

--- src/main/java/eu/scrayos/proxytablist/ProxyTablist.java
+++ src/main/java/eu/scrayos/proxytablist/ProxyTablist.java
@@ -1,5 +1,6 @@
 package eu.scrayos.proxytablist;
 
+import eu.scrayos.proxytablist.handlers.ProxyTablistHandler;
 import eu.scrayos.proxytablist.include.Metrics;
 import eu.scrayos.proxytablist.listeners.ServerConnectListener;
 import net.craftminecraft.bungee.bungeeyaml.pluginapi.ConfigurablePlugin;
@@ -9,8 +10,13 @@ import java.io.IOException;
 
 public class ProxyTablist extends ConfigurablePlugin implements Listener {
 
+    private static ProxyTablist is;
+    private ProxyTablistHandler th;
+
     @Override
     public void onEnable() {
+        is = this;
+        th = new ProxyTablistHandler();
         saveDefaultConfig();
         getProxy().getPluginManager().registerListener(this, new ServerConnectListener());
         try {
@@ -19,5 +25,13 @@ public class ProxyTablist extends ConfigurablePlugin implements Listener {
         } catch (IOException ignored) {
             System.out.println("Failed to initialize Metrics!");
         }
+    }
+
+    public static ProxyTablist getInstance() {
+        return is;
+    }
+
+    public ProxyTablistHandler getTablistHandler() {
+        return th;
     }
 }
---
+++ src/main/java/eu/scrayos/proxytablist/listeners/PostLoginListener.java
@@ -0,0 +1,14 @@
+package eu.scrayos.proxytablist.listeners;
+
+import eu.scrayos.proxytablist.ProxyTablist;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class PostLoginListener implements Listener {
+
+    @EventHandler
+    public void onPostLogin(PostLoginEvent e) {
+        e.getPlayer().setTabList(ProxyTablist.getInstance().getTablistHandler());
+    }
+}
