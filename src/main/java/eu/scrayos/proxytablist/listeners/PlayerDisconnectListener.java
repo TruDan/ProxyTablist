dc402a039e446d4852bf63dcadb6917761d2b6ec
Scrayos <scrayos@justchunks.net>
2014-01-22 23:31:36 +0100

    Fix List just semi-updating on PlayerLeave.

--- src/main/java/eu/scrayos/proxytablist/listeners/PlayerDisconnectListener.java
+++ src/main/java/eu/scrayos/proxytablist/listeners/PlayerDisconnectListener.java
@@ -5,10 +5,19 @@ import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+import java.util.concurrent.TimeUnit;
+
 public class PlayerDisconnectListener implements Listener {
 
     @EventHandler
     public void onPlayerDisconnect(PlayerDisconnectEvent e) {
+        ProxyTablist.getInstance().getProxy().getScheduler().schedule(ProxyTablist.getInstance(), new Runnable() {
+
+            @Override
+            public void run() {
+                ProxyTablist.getInstance().getTablist().refresh();
+            }
+        }, 100, TimeUnit.MILLISECONDS);
         ProxyTablist.getInstance().getTablist().refresh();
     }
 }
