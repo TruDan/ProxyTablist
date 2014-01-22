5fe11ab9077ee481529fab722baf96fce89ecb10
Scrayos <scrayos@justchunks.net>
2014-01-22 23:28:07 +0100

    Fix Checkstyle.

--- src/main/java/eu/scrayos/proxytablist/listeners/PlayerDisconnectListener.java
+++ src/main/java/eu/scrayos/proxytablist/listeners/PlayerDisconnectListener.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class PlayerDisconnectListener implements Listener {
+
     @EventHandler
     public void onPlayerDisconnect(PlayerDisconnectEvent e) {
         ProxyTablist.getInstance().getTablist().refresh();
--- src/main/java/eu/scrayos/proxytablist/listeners/ServerConnectListener.java
+++ src/main/java/eu/scrayos/proxytablist/listeners/ServerConnectListener.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class ServerConnectListener implements Listener {
+
     @EventHandler
     public void onServerConnect(ServerConnectEvent e) {
         ProxyTablist.getInstance().getTablist().refresh();
