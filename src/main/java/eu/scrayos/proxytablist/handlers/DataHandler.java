bdb4a46e400e8221bdb5fd27460275c40728df2a
Scrayos <scrayos@justchunks.net>
2014-01-22 18:26:52 +0100

    Remove old features

--- src/main/java/eu/scrayos/proxytablist/handlers/DataHandler.java
+++ src/main/java/eu/scrayos/proxytablist/handlers/DataHandler.java
@@ -2,7 +2,6 @@ package eu.scrayos.proxytablist.handlers;
 
 import eu.scrayos.proxytablist.ProxyTablist;
 import eu.scrayos.proxytablist.objects.Variable;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.io.File;
 import java.net.URL;
@@ -20,7 +19,28 @@ public class DataHandler {
         variables = new HashSet<>();
         strings = new HashSet<>();
         refreshID = 0;
-        loadVariables();
+        ClassLoader loader = null;
+        try {
+            loader = new URLClassLoader(new URL[]{new File(ProxyTablist.getInstance().getDataFolder() + "/variables").toURI().toURL()}, Variable.class.getClassLoader());
+        } catch (Exception ignored) {
+        }
+        for (File file : new File(ProxyTablist.getInstance().getDataFolder() + "/variables").listFiles()) {
+            try {
+                if (!file.getName().endsWith(".class")) {
+                    continue;
+                }
+                Class<?> aClass = loader.loadClass(file.getName().substring(0, file.getName().lastIndexOf(".")));
+                Object object = aClass.newInstance();
+                if (!(object instanceof Variable)) {
+                    ProxyTablist.getInstance().getLogger().log(Level.WARNING, "Error while loading " + file.getName() + " (No Variable)");
+                    continue;
+                }
+                variables.add((Variable) object);
+            } catch (Exception ignored) {
+                ProxyTablist.getInstance().getLogger().log(Level.WARNING, "Error while loading " + file.getName() + " (Unspecified Error)");
+                ignored.printStackTrace();
+            }
+        }
     }
 
     public int getRefreshID() {
@@ -44,41 +64,7 @@ public class DataHandler {
         strings.clear();
     }
 
-    public String formatName(ProxiedPlayer p) {
-        String name = p.getName();
-        for (String c : new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "l", "m", "n", "o", "k", "r"}) {
-            name = (p.hasPermission("proxy.tablist." + c) ? "§" + c + name : name);
-        }
-        String prefix = ProxyTablist.getInstance().getConfig().getString("prefixes." + p.getServer().getInfo().getName());
-        return (prefix != null ? prefix + name : name);
-    }
-
     public String verifyEntry(String arg) {
         return (arg.length() > 16 ? arg.substring(0, 16) : arg);
-    }
-
-    public void loadVariables() {
-        ClassLoader loader = null;
-        try {
-            loader = new URLClassLoader(new URL[]{new File(ProxyTablist.getInstance().getDataFolder() + "/variables").toURI().toURL()}, Variable.class.getClassLoader());
-        } catch (Exception ignored) {
-        }
-        for (File file : new File(ProxyTablist.getInstance().getDataFolder() + "/variables").listFiles()) {
-            try {
-                if (!file.getName().endsWith(".class")) {
-                    continue;
-                }
-                Class<?> aClass = loader.loadClass(file.getName().substring(0, file.getName().lastIndexOf(".")));
-                Object object = aClass.newInstance();
-                if (!(object instanceof Variable)) {
-                    ProxyTablist.getInstance().getLogger().log(Level.WARNING, "Error while loading " + file.getName() + " (No Variable)");
-                    continue;
-                }
-                variables.add((Variable) object);
-            } catch (Exception ignored) {
-                ProxyTablist.getInstance().getLogger().log(Level.WARNING, "Error while loading " + file.getName() + " (Unspecified Error)");
-                ignored.printStackTrace();
-            }
-        }
     }
 }
