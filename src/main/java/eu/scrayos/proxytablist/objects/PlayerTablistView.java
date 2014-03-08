db2051dd9ce096de228433835d2c8e436b46dd7a
Scrayos <scrayos@justchunks.net>
2014-03-08 22:39:38 +0100

    Fix some things + more sending safety.

--- src/main/java/eu/scrayos/proxytablist/objects/PlayerTablistView.java
+++ src/main/java/eu/scrayos/proxytablist/objects/PlayerTablistView.java
@@ -5,9 +5,6 @@ import eu.scrayos.proxytablist.ProxyTablist;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.protocol.packet.PlayerListItem;
 
-/**
- * @author geNAZt (fabian.fassbender42@googlemail.com)
- */
 public class PlayerTablistView {
     private SlotContainer[] currentView;
     private SlotContainer[] newView;
@@ -32,15 +29,15 @@ public class PlayerTablistView {
      * Sets a specific slot inside the array
      *
      * @param slotNumber number of which slot to set
-     * @param text to set into the slot
-     * @param ping to set into the slot
+     * @param text       to set into the slot
+     * @param ping       to set into the slot
      */
     public void setSlot(int slotNumber, String text, Short ping) {
         Preconditions.checkArgument(slotNumber > -1, "Slot number is under 0");
         Preconditions.checkNotNull(text, "Text can not be null");
 
         //Care that only the lowest number gets marked
-        if(slotNumber < changedSlot) {
+        if (slotNumber < changedSlot) {
             changedSlot = slotNumber;
         }
 
@@ -56,7 +53,7 @@ public class PlayerTablistView {
     private SlotContainer getSlot(int slot) {
         SlotContainer fromGlobal = GlobalTablistView.getSlot(slot);
 
-        if(fromGlobal != null) return fromGlobal;
+        if (fromGlobal != null) return fromGlobal;
         return currentView[slot];
     }
 
@@ -69,7 +66,7 @@ public class PlayerTablistView {
     private SlotContainer getSlotFromNewView(int slot) {
         SlotContainer fromGlobal = GlobalTablistView.getSlotFromNewView(slot);
 
-        if(fromGlobal != null) return fromGlobal;
+        if (fromGlobal != null) return fromGlobal;
         return newView[slot];
     }
 
@@ -77,9 +74,9 @@ public class PlayerTablistView {
      * Empties the Players Tablist
      */
     public void completeClear() {
-        for(int i = 0; i < currentView.length; i++) {
+        for (int i = 0; i < currentView.length; i++) {
             SlotContainer slotValue = getSlot(i);
-            if(slotValue != null)
+            if (slotValue != null)
                 proxiedPlayer.unsafe().sendPacket(new PlayerListItem(slotValue.getText(), false, slotValue.getPing()));
         }
     }
@@ -89,33 +86,33 @@ public class PlayerTablistView {
      */
     public void fireUpdate() {
         //No slot has been changed
-        if(changedSlot == -1) {
+        if (changedSlot == -1) {
             //Send the new Entries
-            for(int i = 0; i < newView.length - 1; i++) {
+            for (int i = 0; i < newView.length - 1; i++) {
                 SlotContainer send = getSlotFromNewView(i);
 
-                if(send != null)
+                if (send != null)
                     proxiedPlayer.unsafe().sendPacket(new PlayerListItem(send.getText(), true, send.getPing()));
             }
 
             changedSlot = 999;
         }
 
-        if(changedSlot == 999) return;
+        if (changedSlot == 999) return;
 
         //Send offline messages to clear out the list
-        for(int i = currentView.length - 1; i > changedSlot - 2; i--) {
+        for (int i = currentView.length - 1; i > changedSlot - 2; i--) {
             SlotContainer send = getSlot(i);
 
-            if(send != null)
+            if (send != null)
                 proxiedPlayer.unsafe().sendPacket(new PlayerListItem(send.getText(), false, send.getPing()));
         }
 
         //Send the new Entries
-        for(int i = changedSlot - 1; i < newView.length; i++) {
+        for (int i = changedSlot - 1; i < newView.length; i++) {
             SlotContainer send = getSlotFromNewView(i);
 
-            if(send != null)
+            if (send != null)
                 proxiedPlayer.unsafe().sendPacket(new PlayerListItem(send.getText(), true, send.getPing()));
         }
 
